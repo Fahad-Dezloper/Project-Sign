57c911cbf7638f87490cfecdcaca6ece4ee1da9d
Fahad <fahad.khan2216@gmail.com>
2025-01-29 14:55:04 +0530

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import React, { FC, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
     WalletModalProvider,
     WalletDisconnectButton,
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
 import RequestAirdrop from './RequestAirdrop';
